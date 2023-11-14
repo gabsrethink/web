9648d8bd9531dd6682907f43779e2eb43b6abdf1
gabriel.melo <gabriel.melo@rethink.dev>
2023-11-14 15:03:17 -0300

    new memory screen

---
+++ src/components/Copyright.tsx
@@ -0,0 +1,9 @@
+import { Heart } from "lucide-react";
+
+export function Copyright() {
+  return (
+    <div className="flex items-center gap-1 text-sm leading-relaxed text-gray-200">
+      Feito com <Heart className="h-4 w-4" /> por Gabriel
+    </div>
+  );
+}
